3e0e3cfc7c60eb34ef4493513a1d9db999690051
Louis Savoie <thelousavoie@gmail.com>
2022-05-24 19:28:07 -0400

    Add games list, adjust header/footer
    Adds the games list to the home page and styles it as well as adjusts
    the header and footer styles to work with the new page content. Nav
    links: new, feedback, and devlog are added to header.

--- partials/header.js
+++ partials/header.js
@@ -2,6 +2,11 @@ const headerDOM = document.querySelector('header')
 const header = () => {
     return `
     <a href="/"><h1>Placeholder</h1></a>
+    <nav>
+        <a href="/testgamepage">NEW!</a>
+        <a href="/feedback">Feedback</a>
+        <a href="/devlog">Devlog</a>
+    </nav>
     `
 }
 headerDOM.innerHTML = header()
