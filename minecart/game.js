19619fa25a2c47a43359ee61a3a191e957f447d1
Louis Savoie <thelousavoie@gmail.com>
2022-05-27 17:12:24 -0400

    Add scoring
    Adds a scoreDisplay, score values to blocks, saves scores to
    localStorage, loads scores from localStorage, displays scores on
    scoresDisplay, and adds initials input for adding your initials to your
    score on the highscores list.

--- minecart/game.js
+++ minecart/game.js
@@ -10,45 +10,63 @@ const blockWeightUnitDisplay = document.getElementById('block-weight-unit-displa
 const minedBlocksDisplay = document.getElementById('mined-blocks-display')
 const currentWeightDisplay = document.getElementById('current-weight-display')
 const maxWeightDisplay = document.getElementById('max-weight-display')
+const scoreDisplay = document.getElementById('score-display')
+const scoresDisplay = document.getElementById('scores-display')
+// Inputs
+const initialsInput = document.getElementById('initials')
 
 // Data
 const blocks = {
     stone: {
+        name: 'stone',
         text: 'You got stone!',
         img: 'img/stone.png',
-        weight: 20
+        weight: 20,
+        score: 1
     },
     copper: {
+        name: 'copper',
         text: 'You got copper!',
         img: 'img/copper.png',
-        weight: 5
+        weight: 5,
+        score: 5
     },
     tin: {
+        name: 'tin',
         text: 'You got tin!',
         img: 'img/tin.png',
-        weight: 3
+        weight: 3,
+        score: 10
     },
     iron: {
+        name: 'iron',
         text: 'You got iron!',
         img: 'img/iron.png',
-        weight: 10
+        weight: 10,
+        score: 20
     },
     silver: {
+        name: 'silver',
         text: 'You got silver!',
         img: 'img/silver.png',
-        weight: 2
+        weight: 2,
+        score: 40
     },
     gold: {
+        name: 'gold',
         text: 'You got gold!',
         img: 'img/gold.png',
-        weight: 1
+        weight: 1,
+        score: 100
     },
     win: {
+        name: 'win',
         text: 'You WIN!',
-        img: 'img/win.png',
+        img: 'img/win.gif',
         weight: ''
     },
     lose: {
+        name: 'lose',
         text: 'You LOSE!',
         img: 'img/lose.png',
         weight: ''
@@ -59,16 +77,34 @@ const maxWeight = 100
 // States
 let minedBlocks = []
 let currentWeight = 0
-maxWeightDisplay.innerText = maxWeight
+let score = 0
+let scores = []
+
+// On load
+getLocalStorage()
+setupDisplays()
 
 // Functions
+function getLocalStorage() {
+    if (localStorage.getItem('minecartScores') === null) {
+        localStorage.minecartScores = JSON.stringify(scores)
+    } else {
+        scores = JSON.parse(localStorage.getItem('minecartScores'))
+    }
+}
+
+function setupDisplays() {
+    if (scores.length != 0) scoresDisplay.innerHTML = scores.map(v => v.element).join('')
+    maxWeightDisplay.innerText = maxWeight
+}
+
 function mine() {
     const random = Math.floor(Math.random() * 100) + 1
-    if (random <= 50) {
+    if (random <= 30) {
         return blocks.stone
-    } else if (random <= 75) {
+    } else if (random <= 50) {
         return blocks.copper
-    } else if (random <= 75) {
+    } else if (random <= 70) {
         return blocks.tin
     } else if (random <= 85) {
         return blocks.iron
@@ -81,7 +117,7 @@ function mine() {
 
 function updateLootDisplay(block) {
     lootText.innerText = block.text
-    blockImg.innerHTML = `<img src="${block.img}">`
+    blockImg.innerHTML = `<img id="block-${block.name}" src="${block.img}">`
     blockWeight.innerText = block.weight
 }
 
@@ -90,6 +126,11 @@ function updateMinecart(block) {
     minedBlocksDisplay.innerHTML = minedBlocks.join('')
     currentWeight += block.weight
     currentWeightDisplay.innerText = currentWeight
+}
+
+function updateScore(block) {
+    score += block.score
+    scoreDisplay.innerText = score
 }
 
 function checkGameState() {
@@ -103,11 +144,19 @@ function checkGameState() {
 function runWin() {
     updateLootDisplay(blocks.win)
     blockWeightUnitDisplay.innerText = 'PERFECTLY LOADED!'
+    saveScore()
 }
 
 function runLose() {
     updateLootDisplay(blocks.lose)
     blockWeightUnitDisplay.innerText = 'OVERLOADED!'
+}
+
+function saveScore() {
+    scores.push({ score: score, element: `<li>${initialsInput.value.toUpperCase()}: ${score}</li>` })
+    scores.sort((a, b) => b.score - a.score)
+    localStorage.minecartScores = JSON.stringify(scores)
+    scoresDisplay.innerHTML = scores.map(v => v.element).join('')
 }
 
 // Event Listeners
@@ -119,6 +168,7 @@ pickaxeImg.addEventListener('mouseup', () => {
     const block = mine()
     updateLootDisplay(block)
     updateMinecart(block)
+    updateScore(block)
     pickaxeImg.src = 'img/pickaxe1.png'
     checkGameState()
 })
